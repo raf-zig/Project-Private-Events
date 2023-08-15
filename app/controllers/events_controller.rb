7e2176de9846346f848eea83122b1f3b024a2d4a
raf-zig <ziravin@yandex.ru>
2023-08-15 23:22:46 +0500

    Add EventsController and corresponding routes, form for creating an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,26 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      flash[:notice] = 'Event successfully created'
+      redirect_to @event
+    else
+      flash.now[:notice] = 'Something went wrong'
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   devise_for :users
-  get 'events/index'
+  #get 'events/index'
+  #get 'events/new'
+  resources :events, only: [:index, :new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
