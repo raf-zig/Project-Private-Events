5f43e8f90007d12682c975635603341be256f271
raf-zig <ziravin@yandex.ru>
2023-08-23 23:42:46 +0500

    Separate the past and upcoming events on the Event’s Index page and on the User’s Show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
-  has_many :event_attendances, foreign_key: :attended_event_id
-  has_many :attendees, through: :event_attendances
+  has_many   :event_attendances, foreign_key: :attended_event_id
+  has_many   :attendees, through: :event_attendances
   belongs_to :creator, class_name: "User"
+
+  scope :past_events, -> { where('date < ?', Date.today) }
+  scope :future_events, -> { where('date >= ?', Date.today) }
 end
