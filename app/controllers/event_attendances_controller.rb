dd5477f80cb2fe29a1706da774271a69c2b7f5b4
raf-zig <ziravin@yandex.ru>
2023-08-24 22:46:12 +0500

    Add deleting yourself to users as a member of their “attended_events”

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,16 +1,23 @@
 class EventAttendancesController < ApplicationController
   def new
-    @eve = EventAttendance.new
+    @event_attendance = EventAttendance.new
   end
 
   def create
-    @eve = EventAttendance.new(attended_event_id: params[:id], attendee_id: current_user.id)
-    @eve.save
+    @event_attendance = EventAttendance.new(attended_event_id: params[:id], attendee_id: current_user.id)
+    @event_attendance.save
     
-    if @eve.save
-      redirect_to events_path
+    if @event_attendance.save
+      redirect_to "/events/#{@event_attendance.attended_event_id}"
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @event_attendance = EventAttendance.find(params[:id])
+    @event_attendance.destroy
+
+    redirect_to "/users/#{current_user.id}"
   end
 end
